9428006a0a75940f327bb6f27cd53a4db5553408
root <abondr>
2018-08-18 19:18:17 +0530

    master initial commit 18-08-2018

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Jquery Basic Examples</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link href="library/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/style01.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <script src="library/js/jquery.min.js" type="text/javascript"></script>
+        <script src="library/js/bootstrap.bundle.js" type="text/javascript"></script>        
+        <script src="assets/js/javascript01.js" type="text/javascript"></script>
+    </body>
+</html>
